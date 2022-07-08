22ebd0d457fd5f3c37e6d97d452a9bce3be90ede
lullimao <102565741+lullimao@users.noreply.github.com>
2022-07-08 00:13:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+const chalk = require("chalk");
+const figlet = require("figlet");
+const inquirer = require("inquirer");
+const request = require("@i-scrapper/plugins-request");
+
+const questions = [
+    {
+        type: "input",
+        name: "authorization",
+        message: color("Authorization keys:"),
+        prefix: `${color("[", "redBright")}+${color("]", "redBright")}`,
+        suffix: "~",
+        validate: function (input) {
+            const done = this.async();
+            if (!input) {
+                done('Please input your auth key!');
+                return false;
+            }
+            let authParse;
+            try {
+                authParse = JSON.parse(input);
+            } catch (error) {
+                authParse = error.message;
+            }
+            if (typeof authParse != "object") {
+                done("Please input your auth key as Obj!");
+                return false;
+            }
+            return done(null, true);
+        },
+    },
+    {
+        type: "list",
+        name: "round",
+        message: color("Authorization keys taken at 'Round':"),
+        prefix: `${color("[", "redBright")}+${color("]", "redBright")}`,
+        suffix: "~",
+        choices: ["Stage 1", "Stage 2", "Stage 3"],
+        filter: (value) => {
+            return {
+                "Stage 1": 1,
+                "Stage 2": 2,
+                "Stage 3": 3,
+            }[value];
+        },
+    },
+    {
+        type: "input",
+        name: "interval",
+        message: color("Delay Interval:"),
+        prefix: `${color("[", "redBright")}+${color("]", "redBright")}`,
+        suffix: "~",
+        default: 1000,
+        validate: function (input) {
+            const done = this.async();
+            if (input && isNaN(input)) {
+                done('Invalid Input!');
+                return false;
+            }
+            return done(null, true);
+            console.clear();
+        },
+    }
+];
+
+const asciiText = figlet.textSync("lol nah", {
+    font: 'Graffiti',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 75,
+    whitespaceBreak: true
+});
+console.log(color(asciiText, "redBright"));
+
+inquirer.prompt(questions)
+    .then(async ({ authorization, round, interval }) => {
+        const authParse = JSON.parse(authorization);
+        iStumble(interval, round, authParse);
+    });
+
+function iStumble(interval, round, authorization) {
+    setInterval(async function iStumble() {
+        try {
+            const { data } = await stageRequest(authorization, round);
+            if (typeof data == "string" && data.includes("BANNED")) {
+                console.error(color("Account Banned!", "redBright"));
+            } else if (typeof data == "object") {
+                const date = new Date();
+                let { Username, Crowns, SkillRating } = data.User;
+                const print = `[${color(date.getHours())}:${date.getMinutes()}] ` + [color('lol nah'), color(Username), color(Crowns, "red"), color(SkillRating, "yellowBright")].join(", ");
+                console.log(print);
+            }
+        } catch (error) {}
+    }, Number(interval));
+}
+
+function color(text, color) {
+    return color ? chalk[color].bold(text) : chalk.white.bold(text);
+}
+
+function stageRequest(authorization, round) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: `http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/${round}`,
+            headers: {
+                Authorization: JSON.stringify(authorization),
+                use_response_compression: true,
+                "Accept-Encoding": "gzip",
+                "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; Redmi 7 MIUI/V11.0.7.0.PFLMIXM)",
+            }
+        })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch(reject);
+    });
+}
